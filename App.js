d9797378400a9ab40f94b239ab6f517dfb3e9c7a
osamakhanedu <osamakhanedu@gmail.com>
2019-10-09 18:31:03 +0500

    backend service for weather forecast added and background image is added

--- App.js
+++ App.js
@@ -1,36 +1,102 @@
 import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ImageBackground
+} from "react-native";
+import Forecast from "./components/Forecast";
+import OpenWeatherMap from "./services/open_weather_map";
+import { AuthSession } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit index.ios.js</Text>
-      <Text style={styles.instructions}>
-        Press Cmd+R to reload,{"\n"}
-        Cmd+D or shake for dev menu
-      </Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: "",
+      forecast: null
+    };
+  }
+
+  _handleTextChange = event => {
+    let zip = event.nativeEvent.text;
+    this.setState({ zipCode: zip });
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      console.log(forecast);
+      this.setState({ forecast: forecast });
+    });
+  };
+
+  render() {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./assets/flowers.png")}
+          resizeMode="cover"
+          style={styles.backdrop}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>Current weather for</Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  keyboardType={"numeric"}
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing={this._handleTextChange}
+                  underlineColorAndroid="transparent"
+                />
+              </View>
+            </View>
+            {content}
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
+
+const baseFontSize = 16;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d7ecf5",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
 
-AppRegistry.registerComponent("FirstProject", () => App);
+    alignItems: "center",
+    paddingTop: 30
+  },
+  backdrop: { flex: 1, width: "100%", flexDirection: "column" },
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: "#000000",
+    opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30
+  },
+  zipContainer: {
+    height: baseFontSize + 10,
+    borderBottomColor: "#DDDDDD",
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: { flex: 1, flexBasis: 1, width: 50, height: baseFontSize },
+  mainText: { fontSize: baseFontSize, color: "#FFFFFF" }
+});
